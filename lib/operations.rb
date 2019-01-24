71cfc1457a9585047c9756680ffd74d4d33c13fc
declarative-spooling-5152 <kerneltux0@gmail.com>
2019-01-24 17:57:08 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,21 @@
 def unsafe?(speed)
-
+  if speed > 60
+    return true
+  elsif speed < 40
+    return true
+  else
+    return false
+  end  
 end
 
 
 
 def not_safe?(speed)
-	
+  if speed > 60
+    return true
+  end  
+  speed < 40 ? "True" : "False"
 end
 	
-
+not_safe?(30)
 
